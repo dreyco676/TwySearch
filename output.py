113591fb3bbfa25830d0707feff5ce4e8d466f04
dreyco676 <dreyco676@gmail.com>
2014-11-01 09:54:30 -0500

    created skeleton classes for transform and output

--- output.py
+++ output.py
@@ -1,1 +1,10 @@
-__author__ = 'dreyco676'
+class transform:
+    def __init__(self,data, out_type):
+        self.data = data
+        self.out_type = out_type
+
+class output:
+    def __init__(self,data,filename,out_dir):
+        self.data = data
+        self.filename = filename
+        self.out_dir = out_dir
