730c8587e1a6a99f0489f3d77eed6978061f707f
dreyco676 <dreyco676@gmail.com>
2014-11-01 14:34:24 -0500

    worked on convert_params

--- search.py
+++ search.py
@@ -1,7 +1,7 @@
 import twython
 
 class TwitterSearch:
-    def __init__(self, keywords, geocode, lang, max_results, max_date):
+    def __init__(self, keywords, geocode=None, lang=None, max_results=None, max_date=None):
         #twitter documentation
         #https://dev.twitter.com/rest/reference/get/search/tweets
         self.keywords = keywords
@@ -22,11 +22,25 @@ class TwitterRequest:
         self.search_params = search_params
         self.session_auth = session_auth
 
+    def convert_params(self):
+        param_list = [self.search_params['keywords']]
+        if self.search_params['geocode'] is not None:
+            param_list.extend('geocode='+str(self.search_params['geocode']))
+        if self.search_params['lang'] is not None:
+            param_list.extend('lang='+str(self.search_params['lang']))
+        if self.search_params['max_results'] is not None:
+            param_list.extend('count='+str(self.search_params['max_results']))
+        if self.search_params['max_date'] is not None:
+            param_list.extend('until='+str(self.search_params['max_date']))
+
+        return param_list
+
     def page_results(self, max_results):
 
     def make_request(self):
         if self.search_params.max_results > 100:
-            page_results(self.search_params.max_results)
-            #do stuff to page
-
-
+            while self.search_params.max_results > 100:
+                twython.search(self.search_params)
+        else:
+            #make request
+            twython.search(self.search_params)
