36181e699edae273450ea49145c52c314f133341
dreyco676 <dreyco676@gmail.com>
2014-11-01 09:55:05 -0500

    created skeleton classes for search

--- search.py
+++ search.py
@@ -1,1 +1,5 @@
-__author__ = 'dreyco676'
+class TwitterSearch:
+    def __init__(self, keywords, params, auth):
+        self.keywords = keywords
+        self.params = params
+        self.auth = auth
