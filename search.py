a11348ca47fe96b518626cc4fb956f011823e8e4
dreyco676 <dreyco676@gmail.com>
2014-11-02 17:23:36 -0600

    removed unnessicary prints

--- search.py
+++ search.py
@@ -13,79 +13,64 @@ class TwitterSearch:
     #KEYWORD
     @property
     def keyword(self):
-        print("Keyword property for search")
         return self._keyword
 
     @keyword.setter
     def keyword(self,value):
-        print("Setter of Keyword")
         self._keyword = value
 
     @keyword.deleter
     def keyword(self):
-        print("Deleter of keyword")
         del self._keyword
 
     #GEOCODE
     @property
     def geocode(self):
-        print("Geocode property for search")
         return self._geocode
 
     @geocode.setter
     def geocode(self,value):
-        print("Setter of Geocode")
         self._geocode = value
 
     @geocode.deleter
     def geocode(self):
-        print("Deleter of geocode")
         del self._geocode
 
     #LANG
     @property
     def lang(self):
-        print("Lang property for search")
         return self._lang
 
     @lang.setter
     def lang(self,value):
-        print("Setter of Lang")
         self._lang = value
 
     @lang.deleter
     def lang(self):
-        print("Deleter of Lang")
         del self._lang
 
     #MAX_RESULTS
     @property
     def max_results(self):
-        print("Max Results property for search")
         return self._max_results
 
     @max_results.setter
     def max_results(self,value):
-        print("Setter of Max Results")
         self._max_results = value
 
     @max_results.deleter
     def max_results(self):
-        print("Deleter of Max Results")
         del self._max_results
 
     #MAX_DATE
     @property
     def max_date(self):
-        print("Max Date property for search")
         return self._max_date
 
     @max_date.setter
     def max_date(self,value):
-        print("Setter of Max Date")
         self._max_date = value
 
     @max_date.deleter
     def max_date(self):
-        print("Deleter of MAx Date")
         del self._max_date
