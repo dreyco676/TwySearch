58ff9f8d2b61bf014dda1a749a0314a9a0e10533
dreyco676 <dreyco676@gmail.com>
2014-11-02 19:10:47 -0600

    added getters and setters. Worked on logic to handle requests over 100

--- request.py
+++ request.py
@@ -1,4 +1,5 @@
 import twython
+import search
 
 class TwitterRequest:
     def __init__(self):
@@ -26,18 +27,48 @@ class TwitterRequest:
         return self._session_auth
 
     @session_auth.setter
-    def search_param(self,value):
+    def session_auth(self,value):
         self._session_auth = value
 
     @session_auth.deleter
-    def search_param(self):
+    def session_auth(self):
         del self._session_auth
 
-
     def make_request(self):
-        if self.search_params._max_results > 100:
-            while self.search_params.max_results > 100:
-                twython.search(self.search_params)
+        api_url = 'https://api.twitter.com/1.1/search/tweets.json'
+        #
+        try:
+            if self._req_param._keyword is None:
+                raise Exception("Keyword Parameter cannot be None!")
+            else:
+                q = self._req_param._keyword
+        except:
+            raise Exception("Keyword Parameter not defined!")
+        try:
+            geocode = self._req_param._geocode
+        except:
+            geocode = None
+        try:
+            lang = self._req_param._lang
+        except:
+            lang = None
+        try:
+            result_type = self._req_param._result_type
+        except:
+            result_type = None
+        try:
+            count = self._req_param._max_results
+        except:
+            count = None
+        try:
+            until = self._req_param._max_date
+        except:
+            until = None
+
+        if count > 100:
+            while count > 100:
+                constructed_url = self.session_auth.construct_api_url(api_url, q=q, geocode=geocode, lang=lang,
+                                                            result_type=result_type, count=100, until=until)
         else:
             #make request
             twython.search(self.search_params)
