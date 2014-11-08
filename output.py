14a16305f40b3c5114392a4a7980f27bbdb0711b
dreyco676 <dreyco676@gmail.com>
2014-11-07 21:19:37 -0600

    added new attributes to FormatOutput class

--- output.py
+++ output.py
@@ -2,6 +2,9 @@ class FormatOutput(object):
     def __init__(self):
         #twitter documentation
         #https://dev.twitter.com/rest/reference/get/search/tweets
+        self._out_dir = None
+        self._file_name = None
+        self._out_type = None
         self._result_set = None
 
     #RESULT_SET
@@ -17,11 +20,50 @@ class FormatOutput(object):
     def result_set(self):
         del self._result_set
 
-    def save_json_file(self, out_dir, file_name):
-        fileName = out_dir + file_name + ".json"
-        f = open(fileName, "wb")
+    #OUT_DIR
+    @property
+    def out_dir(self):
+        return self._out_dir
+
+    @out_dir.setter
+    def out_dir(self,value):
+        self._out_dir = value
+
+    @out_dir.deleter
+    def out_dir(self):
+        del self._out_dir
+
+    #FILE_NAME
+    @property
+    def file_name(self):
+        return self._file_name
+
+    @file_name.setter
+    def out_dir(self,value):
+        self._file_name = value
+
+    @file_name.deleter
+    def file_name(self):
+        del self._file_name
+
+    #OUT_TYPE
+    @property
+    def out_type(self):
+        return self._out_type
+
+    @out_type.setter
+    def out_type(self,value):
+        self._out_type = value
+
+    @out_type.deleter
+    def out_type(self):
+        del self._out_type
+
+    def save_json_file(self):
+        name = self._out_dir + self._file_name + "." + self._out_type
+        f = open(name, "wb")
         f.write(self._result_set)
         f.close()
 
-    def save_delimited_file(self, out_dir, file_name, delimiter):
-        #do stuff
+    def save_csv_file(self):
+        print('#do stuff')
