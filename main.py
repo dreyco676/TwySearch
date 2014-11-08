7ce0186912561ff20806a1f6c36b21a71cc16e3f
dreyco676 <dreyco676@gmail.com>
2014-11-07 22:36:01 -0600

    added output test

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from search import TwitterSearch
 from twython import Twython
 from request import TwitterRequest
+from output import FormatOutput
 
 APP_KEY = '37ZrxAQkWjZi5Mj7euFk0fT2e'
 APP_SECRET = 'Bo5duJpMC3iLHeZuOKyIkZJsOoLcnXbnCfURsrawCZPIp908t3'
@@ -11,12 +12,15 @@ twitter_auth = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 
 search = TwitterSearch()
 search.keyword ='Python'
-search.max_results = 105
+search.max_results = 5
 
 req = TwitterRequest()
 req._req_param = search
 req._session_auth = twitter_auth
 data = req.make_request()
 
-print(len(data["statuses"]))
-print(data)
+save = FormatOutput()
+save._file_name = 'Output'
+save._out_type = 'json'
+save._result_set = data
+save.json_output()
