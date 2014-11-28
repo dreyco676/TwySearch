e2948bb0e7c343e6a64664312219adddd9bc8fb6
dreyco676 <dreyco676@gmail.com>
2014-11-27 22:19:59 -0600

    added and implemented authentication class. Added auth from json file method.

--- auth.py
+++ auth.py
@@ -1,19 +1,87 @@
-import Twython
+from twython import Twython
+import json
 
 class TwitterUser:
-    def __init__(self, name, company, app_key, app_secret, oauth_token, oauth_token_secret):
-        self.name = name
-        self.company = company
-        self.app_key = app_key
-        self.app_secret = app_secret
-        self.oauth_token = oauth_token
-        self.oauth_token_secret = oauth_token_secret
+    def __init__(self):
+        self._app_key = None
+        self._app_secret = None
+        self._oauth_token = None
+        self._oauth_token_secret = None
 
-    def get_auth(self):
-        return Twython(self.app_key, self.app_secret, self.oauth_token, self.oauth_token_secret)
+    #APP_KEY
+    @property
+    def app_key(self):
+        return self._app_key
 
-        #test credentials
-        #APP_KEY = '37ZrxAQkWjZi5Mj7euFk0fT2e'
-        #APP_SECRET = 'Bo5duJpMC3iLHeZuOKyIkZJsOoLcnXbnCfURsrawCZPIp908t3'
-        #OAUTH_TOKEN = '39855951-6MtVemzUgai6hQRMOhNVldha8Fa8nAQoNmfmZYqII'
-        #OAUTH_TOKEN_SECRET = '6I3GlIJ1o4T0DB634KcQRQYghreLkv3jGmeZLIV3Amw68'
+    @app_key.setter
+    def app_key(self, value):
+        self._app_key = value
+
+    @app_key.deleter
+    def app_key(self):
+        del self._app_key
+
+    #APP_SECRET
+    @property
+    def app_secret(self):
+        return self._app_secret
+
+    @app_secret.setter
+    def app_secret(self, value):
+        self._app_secret = value
+
+    @app_secret.deleter
+    def app_secret(self):
+        del self._app_secret
+
+    #OAUTH_TOKEN
+    @property
+    def oauth_token(self):
+        return self._oauth_token
+
+    @oauth_token.setter
+    def oauth_token(self, value):
+        self._oauth_token = value
+
+    @oauth_token.deleter
+    def oauth_token(self):
+        del self._oauth_token
+
+    #OAUTH_TOKEN_SECRET
+    @property
+    def oauth_token_secret(self):
+        return self._oauth_token_secret
+
+    @oauth_token_secret.setter
+    def oauth_token_secret(self, value):
+        self._oauth_token_secret = value
+
+    @oauth_token_secret.deleter
+    def oauth_token_secret(self):
+        del self._oauth_token_secret
+
+    def read_json(self):
+        json_data = open('auth.json')
+        data = json.load(json_data)
+        try:
+            self._app_key = data["app_key"]
+        except:
+            print("Error reading app_key")
+        try:
+            self._app_secret = data["app_secret"]
+        except:
+            print("Error reading app_secret")
+        try:
+            self._oauth_token = data["oauth_token"]
+        except:
+            print("Error reading oauth_token")
+        try:
+            self._oauth_token_secret = data["oauth_token_secret"]
+        except:
+            print("Error reading oauth_token_secret")
+        json_data.close()
+
+    def auth(self):
+        twitter_auth = Twython(self._app_key, self._app_secret, self._oauth_token, self._oauth_token_secret)
+        return twitter_auth
+
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from search import TwitterSearch
-from twython import Twython
 from request import TwitterRequest
 from output import FormatOutput
+from auth import TwitterUser
 from tkinter import *
 class Application (Frame):
 
@@ -41,7 +41,7 @@ class Application (Frame):
 
         req = TwitterRequest()
         req._req_param = search
-        req._session_auth = twitter_auth
+        req._session_auth = auth
         data = req.make_request()
         save = FormatOutput()
         save._file_name = 'Output'
@@ -53,18 +53,14 @@ class Application (Frame):
 
 
 
-        #self.text.insert(data)
-
-#def download
-APP_KEY = '37ZrxAQkWjZi5Mj7euFk0fT2e'
-APP_SECRET = 'Bo5duJpMC3iLHeZuOKyIkZJsOoLcnXbnCfURsrawCZPIp908t3'
-OAUTH_TOKEN = '39855951-6MtVemzUgai6hQRMOhNVldha8Fa8nAQoNmfmZYqII'
-OAUTH_TOKEN_SECRET = '6I3GlIJ1o4T0DB634KcQRQYghreLkv3jGmeZLIV3Amw68'
-twitter_auth = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+#set user
+user = TwitterUser()
+user.read_json()
+auth = user.auth()
 
 root = Tk()
 root.title("Twitter Search Downloader")
-root.geometry("500x200")
+root.geometry("1024x768")
 app = Application(root)
 root.mainloop()
 
