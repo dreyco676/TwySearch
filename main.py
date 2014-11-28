b3664afe6788ceb3339e157c0c84afb84fc8f50b
dreyco676 <dreyco676@gmail.com>
2014-11-28 15:15:09 -0600

    basic GUI layout completed

--- main.py
+++ main.py
@@ -9,25 +9,85 @@ class Application (Frame):
         self.create_widgets()
 
     def create_widgets(self) :
-        #Search keyword
-        self.instruction= Label(self,text="Enter Keyword")
-        self.instruction.grid(row=0, column=0, columnspan=1, sticky=W)
-        self.keyword = Entry(self)
-        self.keyword.grid(row=0, column=1, columnspan=1, sticky=W)
 
-        #Max Results
-        self.max_result = Label(self,text="Enter Maximum Result")
-        self.max_result.grid(row=1, column=0, columnspan=1, sticky=W)
-        self.max_result2 = Entry(self)
-        self.max_result2.grid(row=1, column=1, columnspan=1, sticky=W)
+        #Search keyword
+        self.keyword_desc = Label(self,text="Enter Keyword")
+        self.keyword_desc.grid(row=0, column=0, columnspan=1, sticky=W)
+        self.keyword = Entry(self)
+        self.keyword.grid(row=1, column=0, columnspan=1, sticky=W)
+
+        #Platform
+        self.platform_desc = Label(self,text="Select a platform")
+        self.platform_desc.grid(row=0, column=2, columnspan=1, sticky=W)
+        self.platform = Entry(self)
+        self.platform.grid(row=1, column=2, columnspan=1, sticky=W)
+
+        #ADVANCED OPTIONS
+        self.advanced_opts_div = Label(self,text="ADVANCED OPTIONS")
+        self.advanced_opts_div.grid(row=4, column=0, columnspan=3)
+
+        #MAX_Results
+        self.max_results_desc = Label(self,text="Maximum Results")
+        self.max_results_desc.grid(row=7, column=0, columnspan=1, sticky=W)
+        self.max_results = Entry(self)
+        self.max_results.grid(row=8, column=0, columnspan=1, sticky=W)
+
+        #Geocode
+        self.geocode_desc = Label(self,text="Select Geocode")
+        self.geocode_desc.grid(row=7, column=1, columnspan=1, sticky=W)
+        self.geocode = Entry(self)
+        self.geocode.grid(row=8, column=1, columnspan=1, sticky=W)
+
+        #Lang
+        self.lang_desc = Label(self,text="Select Language")
+        self.lang_desc.grid(row=7, column=2, columnspan=1, sticky=W)
+        self.lang = Entry(self)
+        self.lang.grid(row=8, column=2, columnspan=1, sticky=W)
+
+        #Result_type
+        self.lang_desc = Label(self,text="Select Result Type")
+        self.lang_desc.grid(row=9, column=0, columnspan=1, sticky=W)
+        self.lang = Entry(self)
+        self.lang.grid(row=10, column=0, columnspan=1, sticky=W)
+
+        #Max Date
+        self.max_date_desc = Label(self,text="Enter Max Date (YYYY-MM-DD)")
+        self.max_date_desc.grid(row=9, column=1, columnspan=1, sticky=W)
+        self.max_date = Entry(self)
+        self.max_date.grid(row=10, column=1, columnspan=1, sticky=W)
+
+        #output OPTIONS
+        self.advanced_opts_div = Label(self,text="OUTPUT OPTIONS")
+        self.advanced_opts_div.grid(row=13, column=0, columnspan=3)
+
+
+        #File Name
+        self.file_name_desc = Label(self,text="Enter Output File Name")
+        self.file_name_desc.grid(row=16, column=2, columnspan=1, sticky=W)
+        self.file_name = Entry(self)
+        self.file_name.grid(row=17, column=2, columnspan=1, sticky=W)
+
+        #Save Directory
+        self.out_dir_desc = Label(self,text="Output Directory")
+        self.out_dir_desc.grid(row=16, column=0, columnspan=1, sticky=W)
+        self.out_dir = Entry(self)
+        self.out_dir.grid(row=17, column=0, columnspan=1, sticky=W)
+
+        #Output_type
+        self.out_type_desc = Label(self,text="Select an Output Type")
+        self.out_type_desc.grid(row=16, column=1, columnspan=1, sticky=W)
+        self.out_type = Entry(self)
+        self.out_type.grid(row=17, column=1, columnspan=1, sticky=W)
+
 
         #Execute search
-        self.search_button = Button(self, text="Search", command=self.reveal)
-        self.search_button.grid(row=2, column=0, sticky=W)
+        self.search_button = Button(self, text="Search", command=self.start_search)
+        self.search_button.grid(row=20, column=0, columnspan=3)
 
-    def reveal(self):
+
+    def start_search(self):
         search = Search()
-        search.keyword= self.keyword.get()
+        search.keyword = self.keyword.get()
         #print(self.max_result2.get())
         search.max_results =int(self.max_result2.get())
         search.platform = 'Twitter'
@@ -43,7 +103,7 @@ class Application (Frame):
 
 root = Tk()
 root.title("Twitter Search Downloader")
-root.geometry("1024x768")
+root.geometry("800x600")
 app = Application(root)
 root.mainloop()
 
